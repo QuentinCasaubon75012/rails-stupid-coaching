1dc4f91e27e74d7872bef3c878dcc43830305b84
Quentin Casaubon <quentincasaubonweb@gmail.com>
2017-02-06 14:10:18 +0100

    css

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,16 @@
+class CoachingController < ApplicationController
+  def answer
+    @query = params[:query]
+    if @query.include?("?")
+      return @coach_answer = "Silly question, get dressed and go to work!"
+    elsif @query == "I am going to work right now!"
+      return @coach_answer = ""
+    else
+      return @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def ask
+  end
+
+end
